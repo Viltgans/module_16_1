231caa5f0e1ba3bf6e58e1e471bdcd5d2aa2c0ba
Viltgans <80921674+Viltgans@users.noreply.github.com>
2024-12-09 14:04:08 +0300

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -4,19 +4,19 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"Главная страница"}
+    return "Главная страница"
 
 @app.get("/user/admin")
 async def admin_page():
-    return {"Вы вошли как администратор"}
+    return "Вы вошли как администратор"
 
 @app.get("/user/{user_id}")
 async def user_id_page(user_id: int):
-    return {f"Вы вошли как пользователь № {user_id}"}
+    return f"Вы вошли как пользователь № {user_id}"
 
 @app.get("/user")
 async def user_inform_page(username: str, age: int):
-    return {f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
 
 ## Запуск:
 ## 1. Переход в директорию: cd module_16/homework_16_1/
